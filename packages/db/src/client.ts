d6720ef25134f5c15906c4004c3f5a88d773fa7e
andrewdoro <elfandreis@gmail.com>
2025-01-04 16:56:04 +0200

    init: initial setup uwu stack

---
+++ packages/db/src/client.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
+import { Resource } from "sst";
+
+import * as schema from "./schema";
+
+const pool = new pg.Pool({
+  host: Resource.TSSDatabase.host,
+  port: Resource.TSSDatabase.port,
+  user: Resource.TSSDatabase.username,
+  password: Resource.TSSDatabase.password,
+  database: Resource.TSSDatabase.database,
+});
+
+export const db = drizzle(pool, { schema });
---
+++ sst.config.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "tss",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      providers: {
+        aws: {
+          profile: process.env.GITHUB_ACTIONS
+            ? undefined
+            : input?.stage === "production"
+              ? "acme-production"
+              : "acme-dev",
+        },
+      },
+    };
+  },
+  async run() {
+    const isPermanentStage =
+      $app.stage === "production" || $app.stage === "dev";
+    const betterAuthSecret = new sst.Secret("BetterAuthSecret");
+
+    const vpc = isPermanentStage
+      ? new sst.aws.Vpc("TSSVpc", { bastion: true, nat: "ec2" })
+      : sst.aws.Vpc.get("TTSVpc", "vpc-057d1174bade06382");
+
+    const database = isPermanentStage
+      ? new sst.aws.Postgres("TSSDatabase", {
+          vpc: vpc as sst.aws.Vpc,
+          proxy: true,
+        })
+      : sst.aws.Postgres.get("TSSDatabase", {
+          id: "tss-dev-tssdatabaseinstance",
+          proxyId: "tss-dev-tssdatabaseproxy",
+        });
+
+    const webApp = new sst.aws.TanstackStart("TSSWebApp", {
+      link: [database, betterAuthSecret],
+      vpc,
+      dev: { command: "pnpm run dev:app" },
+    });
+
+    if ($app.stage === "production") {
+      const databasePush = new sst.aws.Function("TSSDatabasePush", {
+        handler: "app/database/database-push.databasePush",
+        link: [database],
+        vpc,
+      });
+
+      new aws.lambda.Invocation("TSSDatabasePushInvocation", {
+        functionName: databasePush.name,
+        input: JSON.stringify({
+          now: new Date().toISOString(),
+        }),
+      });
+    }
+
+    new sst.x.DevCommand("Studio", {
+      link: [database],
+      dev: {
+        command: "pnpm db:studio",
+        autostart: true,
+      },
+    });
+
+    return {
+      webApp: webApp.url,
+    };
+  },
+});
